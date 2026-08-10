a949d6426c5005b77c7f00076cc7e651154d76b6
adin-code1 <lamv123t@gmail.com>
2026-08-11 01:10:10 +0700

    add test gidoc

---
+++ codecpp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << " hello world " << endl;
+    int v = 10;
+    int c = v++;
+    cout << v << endl;
+    cout << c << endl;
+    //..
+    return 0;
+}
