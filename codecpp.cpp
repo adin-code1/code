04eb0cbff85292a2ad41b12f8e0b653ea8cb0b2a
adin-code1 <lamv123t@gmail.com>
2026-08-11 01:57:11 +0700

    clear conflict for real

--- codecpp.cpp
+++ codecpp.cpp
@@ -3,14 +3,10 @@ using namespace std;
 
 int main(){
     cout << " hello world " << endl;
-<<<<<<< HEAD
     int v = 10;
     int c = v++;
     cout << v << endl;
     cout << c << endl;
     //..
-=======
-    // .
->>>>>>> 325dc4ee0051a96ef83528089f530076a95fc92b
     return 0;
 }
