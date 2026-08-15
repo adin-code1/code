583bb329f6227d15efd405507e08b36cc1205871
adin-code1 <lamv123t@gmail.com>
2026-08-15 23:43:17 +0700

    update

--- code.cpp
+++ code.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 int main()
@@ -7,17 +8,20 @@ int main()
     int l = 100;
     cout << l << endl;
 
+        //toan tu toan hc
     int a = 100, b = 200;
     a += b;
     cout << a << endl;
     a %= b;
     cout << a << endl;
 
+        //ep kieu
     float r = 100;
     int q = 200;
     r /= (float)q;
     cout << r << endl;
 
+        //tang tuoc, tang sau
     int ab = 400;
     int kc = ++ ab;
     cout << ab << endl;
@@ -28,6 +32,7 @@ int main()
     cout << mn << endl;
     cout << om << endl;
 
+        //toan tu so sanh + toan tu logic
     int u = 400, t = 500, v = 300, p = 900;
     bool n = (u < t) && (p >= t);
     cout << n << endl;
@@ -35,5 +40,9 @@ int main()
     cout << w << endl;
     bool y = !(u < t);
     cout << y << endl;
+
+        //kiem soat sau . la bao nhieu so
+    float it = 10.85f;
+    cout << fixed << setprecision(2) << it << endl;
     return 0;
 }
