9cc7b829f3f93e6ecc48f1eeef5e7c235d6aa5ad
adin-code1 <lamv123t@gmail.com>
2026-08-18 21:44:01 +0700

    update

--- code.cpp
+++ code.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<iomanip>
+#include<math.h>
 using namespace std;
 
 int main()
@@ -34,8 +35,8 @@ int main()
 
         //toan tu so sanh + toan tu logic
     int u = 400, t = 500, v = 300, p = 900;
-    bool n = (u < t) && (p >= t);
-    cout << n << endl;
+    bool z = (u < t) && (p >= t);
+    cout << z << endl;
     bool w = (u < t) || (p >= t);
     cout << w << endl;
     bool y = !(u < t);
@@ -44,5 +45,48 @@ int main()
         //kiem soat sau . la bao nhieu so
     float it = 10.85f;
     cout << fixed << setprecision(2) << it << endl;
+
+    int vy = -800;
+    int pn = abs(vy);
+    cout << " pn " << endl;
+    int g = 40, e = 9;
+    int h = pow(g , e);
+    cout << h << endl;
+    
+    int i = sqrt(e);
+    cout << i << endl;
+    float x = 79.54f;
+
+    int m = round(x);
+    cout << m << endl;
+
+    int N;
+    cin >> N;
+    if((N >= 5) && (N <= 10)){
+        cout << " on " << endl;
+    }
+    else if((N < 5) && (N > 0)){
+        cout << " thap " << endl;
+    }
+    else if((N > 10) && (N < 50)){
+        cout << " cao " << endl;
+    }
+    else{
+        cout << " du lieu ko hop le " << endl;
+    }
+
+    int n;
+    cin >> n;
+    switch ((int)(n)){
+        case 1: case 3: case 5: case 7: case 9:
+        cout << " le \n ";
+        break;
+        case 2: case 4: case 6: case 8: case 10:
+        cout << " chan \n ";
+        break;
+        default:
+        cout << " du lieu ko hop le \n";
+    }
+
     return 0;
 }
