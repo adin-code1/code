76cf72f76f0816be7cb0dc2d9a8d12971c9af9fe
adin-code1 <lamv123t@gmail.com>
2026-08-20 17:22:30 +0700

    update

--- bai_tap1-20_1.1.cpp
+++ bai_tap1-20_1.1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<math.h>
+#include<iomanip>
 using namespace std;
 
 void bai_1(){
@@ -11,10 +12,31 @@ void bai_1(){
     
 }
 
-int main(){
+void bai_2(){
     cout << " nhap gia tri cua x vao ";
     long long x;
     cin >> x;
     long long h = x * x * x + 3 * x * x + x + 1;
     cout << h << endl;
+}
+
+void bai_3(){
+    int a, b, c;
+    a = abs(a);   b = abs(b);    c = abs(c);
+    cin >> a >> b >> c;
+    int S = a*(b + c) + b*(a + c);
+    cout << S << endl;
+    
+}
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    
+    int tong = a + b;
+    int hieu = a - b;
+    int tich = a * b;
+    float thuong = (float)a/b;
+    cout << tong << " " << hieu << " " << tich << " " << fixed << setprecision(2) << thuong << endl;
+    
 }
