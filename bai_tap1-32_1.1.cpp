e75553c92feecf8eb5f7e522de27d44dc91c53ea
adin-code1 <lamv123t@gmail.com>
2026-08-27 20:44:55 +0700

    update

--- bai_tap1-32_1.1.cpp
+++ bai_tap1-32_1.1.cpp
@@ -274,3 +274,24 @@ void bai_25(){
     
 }
 
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a + b > c){
+        if( a == b == c == a){
+            cout << " 1 " << endl;
+        }
+        else if((a == b) || (b == c) || ( a == c)){
+            cout << " 2 " << endl;
+        }
+        else if((a * a) + (b * b) == (c * c)){
+            cout << " 3 " << endl;
+        }
+        else{
+            cout << " 4 " << endl;
+        }
+    }
+    else{
+        cout << " INVALID " << endl;
+    }
+}
