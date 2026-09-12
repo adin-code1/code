2bd37770c1fd2174fb2390f92c84e38c5353424f
adin-code1 <lamv123t@gmail.com>
2026-09-12 10:40:49 +0700

    update

--- bai_tap_buoi2_1-11.cpp
+++ bai_tap_buoi2_1-11.cpp
@@ -19,11 +19,36 @@ void bai_tap_50A(){
     cout << hinh << endl;
 }
 
-int main(){
+void bai_tap_1A(){
     int n, m, a;
     cin >> n >> m >> a;
     long long q = (n + a -1)/a;
     long long w = (m + a -1)/a;
     long long k = q + w;
     cout << k << endl;
+}
+
+void bai_tap_frog(){
+    long long a, b, k;
+    cin >> a >> b >> k;
+    if(k % 2 == 0){
+        int h = k / 2 * (a-b);
+        cout << h << endl;
+    }
+    else{
+        int g = k / 2 * (a-b) + a;
+        cout << g << endl;
+    }
+}
+
+int main(){
+    int s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+    int a = abs(s1), b = abs(s2), s = abs(s3);
+    if (a + b <= s && s % 2 == 0){
+        cout << " YES " << endl;
+    }
+    else{
+        cout << " NO " << endl;
+    }
 }
