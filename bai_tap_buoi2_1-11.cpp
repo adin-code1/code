e42b9c0bebe5a89fce51b95a794e88dc3d078afb
adin-code1 <lamv123t@gmail.com>
2026-09-12 10:22:17 +0700

    update

--- bai_tap_buoi2_1-11.cpp
+++ bai_tap_buoi2_1-11.cpp
@@ -3,6 +3,12 @@
 #include<iomanip>
 using namespace std;
 
-int main(){
-    
+void bai_tap_22A(){
+    long long a, b, c;
+    cin >> a >> b >> c;
+    long long v = sqrt(a * b * c);
+    long long d = v/a, r = v/b, h = v/c;
+    long long f = 4*(d + r + h);
+    cout << f << endl;
 }
+
